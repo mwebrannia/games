d53454542a1b4898874500d6d1463bc07ea4fafa
Mwebrania <mwebrannia@gmail.com>
2023-10-24 20:56:25 +0300

    first commit

---
+++ number_guessing.py
@@ -0,0 +1,17 @@
+import random
+#you can pass these message in the variables below right inside the print function
+#but i decided to put them in a variable
+small = 'is small'
+large = 'is large'
+number = random.randrange(1, 10)
+guess = int(input('Enter any number: '))
+while number != guess:
+    if guess < number:
+        print(guess, small)
+        guess = int(input('Enter number again: '))
+    elif guess > number:
+        print(guess, large)
+        guess = int(input('Enter number again: '))
+    else:
+        break
+print('You guessed it right,', guess, '=', number)
